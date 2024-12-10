8239e60dfd2d6c116fa952abaf0cb2504dc24d87
martinasartori97 <martinasartori9@gmail.com>
2024-12-10 17:56:56 +0100

    add show

--- controllers/moviecontroller.js
+++ controllers/moviecontroller.js
@@ -1,4 +1,4 @@
-
+const connection = require('../database/connection')
 function index(req, res) {
     connection.query(`SELECT* FROM movies`, (err, results) => {
         if (err) return res.status(500).json({ err: err })
@@ -11,7 +11,40 @@ function index(req, res) {
 }
 
 
+function show(req, res) {
+    const id = req.params.id
+    const sql = `SELECT * FROM movies WHERE id = ?`;
+    const reviewssql = `SELECT * FROM reviews WHERE movie_id = ?`;
+
+
+    connection.query(sql, [id], (err, results) => {
+        if (err) return res.status(500).json({ err: err })
+        if (results.lenght == 0) return res.status(404).json({ err: 'movie not found' })
+
+        console.log(results);
+
+        connection.query(reviewssql), [id], (err, reviewsResults) => {
+            if (err) return res.status(500).json({ err: err })
+            console.log('reviews', reviewsResults);
+
+        }
+
+        const movie = {
+            ...results[0],
+
+        }
+        res.json({
+            movie
+        })
+
+
+
+    })
+}
+
+
 module.exports = {
     index,
+    show
 
 }
--- routes/movies.js
+++ routes/movies.js
@@ -1,56 +1,12 @@
 const express = require('express');
 const router = express.Router();
-const connection = require('../database/connection')
+const MoviesController = require('../controllers/moviecontroller')
 
 
 
-router.get('/', (req, res) => {
+router.get('/', MoviesController.index)
+router.get('/:id', MoviesController.show)
 
-    const sql = `SELECT * FROM movies`;
-
-    connection.query(sql, (err, results) => {
-        console.log(results);
-
-
-
-        res.json({
-            movies: results,
-            count: results.lenght
-        })
-    })
-})
-
-router.get('/:id', (req, res) => {
-
-    const id = req.params.id
-    const sql = `SELECT * FROM movies WHERE id = ?`;
-    const reviewssql = `SELECT * FROM reviews WHERE movie_id = ?`;
-
-
-    connection.query(sql, [id], (err, results) => {
-        if (err) return res.status(500).json({ err: err })
-        if (results.lenght == 0) return res.status(404).json({ err: 'movie not found' })
-
-        console.log(results);
-
-        connection.query(reviewssql), [id], (err, reviewsResults) => {
-            if (err) return res.status(500).json({ err: err })
-            console.log('reviews', reviewsResults);
-
-        }
-
-        const movie = {
-            ...results[0],
-
-        }
-        res.json({
-            movie
-        })
-
-
-
-    })
-})
 
 
 
